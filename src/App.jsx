e274c8fc7095925a2356cd583874953dac53b3d1
Abdulmajid Yunusa <72697280+amjeed-ay@users.noreply.github.com>
2023-07-20 21:53:38 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,19 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdulmajid Yunusa",
+  title: "Software Developer",
+  email: "ay_amjeed@outlook.com",
+  gitHub: "amjeed-ay",
+  instagram: "techjeed",
+  linkedIn: "ay-amjeed",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "techjeed",
+  youTube: "techjeed",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#3ABAB4";
+const secondaryColor = "#3ABAB4";
 
 const App = () => {
   return (
